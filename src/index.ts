83180df68e205e7ef751666fc28bbb4470e8cd4d
huukhuong <huukhuong.it@gmail.com>
2023-06-19 15:56:15 +0700

    update config code

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,25 @@
+import Config from "./types";
+
+const defaultConfig: Config = {
+  fixed: 0,
+  decimalSeparator: '.',
+  shorten: false,
+};
+
 const formatNumber = (
   num: number | string | undefined,
-  fixed = 0,
-  shorten = false
+  config = defaultConfig
 ) => {
   if (num === undefined || num === '') {
     return '0';
   }
 
+  const {fixed, shorten, decimalSeparator} = config;
+
   const parsedNum = typeof num === 'string' ? parseInt(num) : num;
 
   if (!shorten) {
-    return parsedNum.toLocaleString(undefined, {
-      minimumFractionDigits: fixed,
-      maximumFractionDigits: fixed,
-    });
+    return parsedNum.toFixed(fixed).replace(/(\d)(?=(\d{3})+(?!\d))/g, `$1${decimalSeparator}`);
   } else {
     const suffixes = ['', 'K', 'M', 'B', 'T'];
     let suffixIndex = 0;
